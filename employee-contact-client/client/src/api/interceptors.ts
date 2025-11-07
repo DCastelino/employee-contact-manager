39c0cad451ffff83f1b3c09f3a2373fd39538fcb
davidcastelino <david.castelino@gmail.com>
2025-11-07 14:16:28 -0500

    Updating URL to match local dev, and packages

--- employee-contact-client/client/src/api/interceptors.ts
+++ employee-contact-client/client/src/api/interceptors.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:7120',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5039',
   headers: {
     'Content-Type': 'application/json',
   },
